7c93becc58fb67a7517032b4d79bdef6ff992aaa
mgi166 <skskoari@gmail.com>
2013-12-23 16:54:50 +0900

    * I forget some provisioners    * add setup_ssh.sh and vb.sh

---
+++ templates/CentOS-6.4-x86_64-minimal/vb.sh
@@ -0,0 +1,9 @@
+# Installing the virtualbox guest additions
+#VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+#mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+#rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+rm -rf /home/vagrant/VBoxGuestAdditions.iso
