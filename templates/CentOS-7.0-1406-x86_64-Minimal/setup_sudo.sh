779780a276329e95f3713b104c2b56d0f647515c
mgi166 <skskoari@gmail.com>
2014-09-21 18:25:55 +0900

    * Add shell script at `provisioners`

---
+++ templates/CentOS-7.0-1406-x86_64-Minimal/setup_sudo.sh
@@ -0,0 +1,7 @@
+# setup sudo
+/usr/bin/yum -y install sudo
+/usr/sbin/groupadd vagrant
+/usr/sbin/useradd vagrant -g vagrant -G wheel
+echo "vagrant"|passwd --stdin vagrant
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
---
+++ templates/CentOS-7.0-1406-x86_64-Minimal/vb.sh
@@ -0,0 +1,6 @@
+# Installing the virtualbox guest additions
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions.iso
